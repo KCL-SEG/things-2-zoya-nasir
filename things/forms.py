05427d4723b8e5df09b7b7b399875106f4639f23
Zoya Nasir <k22031112@kcl.ac.uk>
2023-12-01 18:48:46 +0000

    final commit

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': 40, 'rows': 5}),
+            'quantity': forms.NumberInput()
+        }
--- things/views.py
+++ things/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .forms import ThingForm  
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()  
+    else:
+        form = ThingForm()
+
     return render(request, 'home.html', {'form': form})
